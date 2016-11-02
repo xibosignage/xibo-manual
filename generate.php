fe9e04de1bba5ec39a0125423a4e57e07bcbef41
Dan Garner <dan@springsignage.com>
2016-11-02 08:06:59 +0000

    Update generate.php to allow overriding the footer.

--- generate.php
+++ generate.php
@@ -3,8 +3,13 @@ include_once('libraries/parsedown/parsedown.php');
 include_once('manualgenerator.class.php');
 include_once('template/template.php');
 
+$argv = $GLOBALS['argv'];
+
+$footer = isset($argv[0]) ? $argv[0] : null;
+
 // Generates a complete manual.
 $manual = new ManualGenerator(PRODUCT_NAME, PRODUCT_HOME, PRODUCT_SUPPORT_URL, PRODUCT_FAQ_URL);
+$manual->overrideFooter = $footer;
 $manual->build('', 'output/');
 
 ?>
